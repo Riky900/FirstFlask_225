10a48380d1ef16338888ac25441eabc60f5e917c
Riky900 <rickytripamungkas666@gmail.com>
2025-12-23 14:12:49 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+import time
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    hasil = None
+    if request.method == "POST":
+        time.sleep(1.5)
+        nama = request.form.get("nama")
+        hasil = f"> HALLO DECK :: {nama}"
+    return render_template("index.html", hasil=hasil)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5002)
